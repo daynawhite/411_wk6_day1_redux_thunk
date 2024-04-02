d2592c3bea6962d526f7bfda89d5346479087f44
daynawhite <dayna26.2@gmail.com>
2024-04-01 21:03:16 -0500

    import button working, finished through line 38 of README

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {
+    Button,
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow
+} from '@mui/material';
 
 const Import = (props) => {
-    // fill out this component
-
+    console.log(props.makes)
     return (
-        <p>Import Component</p>
+        <Container maxWidth="lg" className="car-container">
+            <Button onClick={props.fetchMakes} variant='contained' color='primary'>Import</Button>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {props.makes.map((make) => (
+                    <TableRow key={make.MakeId}>
+                        <TableCell component="th" scope="row">
+                            {make.MakeId}
+                        </TableCell>
+                        <TableCell>{make.MakeName}</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </Container>
     )
 }
 
